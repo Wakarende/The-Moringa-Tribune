4cbf3c59a58cba5871f775050ff4f2f77f122134
Joy Kirii <joykirii@gmail.com>
2021-05-11 16:04:31 +0300

    create test for article model

--- news/urls.py
+++ news/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns=[
-  path('',views.welcome,name = 'welcome'),
-  path('today/',views.news_of_day,name = 'newsToday'),
+  path('',views.news_of_day,name = 'newsToday'),
   re_path('archives/(\d{4}-\d{2}-\d{2})/',views.past_days_news,name = 'pastNews') 
 ]
--- tribune/urls.py
+++ tribune/urls.py
@@ -18,6 +18,6 @@ from django.urls import path,include
 # from django.conf.urls import url,include
 
 urlpatterns = [
-    path('news/', include('news.urls')),
+    path('', include('news.urls')),
     path('admin/', admin.site.urls)    
 ]
