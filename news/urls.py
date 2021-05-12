4f57e0cabd0bb462e7ffa67209ab6b8c3e79693e
Joy Kirii <joykirii@gmail.com>
2021-05-12 18:51:26 +0300

    add upload image functionality

--- news/urls.py
+++ news/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, re_path
-# from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
 urlpatterns=[
@@ -7,3 +8,6 @@ urlpatterns=[
   re_path('archives/(\d{4}-\d{2}-\d{2})/',views.past_days_news,name = 'pastNews'),
   path('search/', views.search_results, name='search_results')
 ]
+
+if settings.DEBUG:
+  urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
