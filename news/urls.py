97873353bfb26dc407d4372bd132d1d448312c83
Joy Kirii <joykirii@gmail.com>
2021-05-18 08:22:30 +0300

    creae newsletter model

--- news/urls.py
+++ news/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from . import views
 
 urlpatterns=[
-  path('',views.news_of_day,name = 'newsToday'),
+  path('',views.news_today,name = 'newsToday'),
   re_path('archives/(\d{4}-\d{2}-\d{2})/',views.past_days_news,name = 'pastNews'),
   path('search/', views.search_results, name='search_results'),
   path('article/(\d+)',views.article,name ='article')
